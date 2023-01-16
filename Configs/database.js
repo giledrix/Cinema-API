a9c86694a6ec30618ff9d9e86de7de086ef7bae4
Gil Edri <giledrix@gmail.com>
2023-01-16 21:56:42 +0200

    Ready to Deploy back end

--- Configs/database.js
+++ Configs/database.js
@@ -1,3 +1,15 @@
-const mongoose = require('mongoose'); // import "mongoose" model.
+// const mongoose = require('mongoose'); // import "mongoose" model.
 
-mongoose.connect('mongodb://localhost:27017/UseerssDB'); // connect to usersDB collection.
+// mongoose.connect('mongodb://localhost:27017/UseerssDB'); // connect to usersDB collection.
+
+const mongoose = require('mongoose')// import "mongoose" model.
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/UseerssDB')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+module.exports = connectDB
--- main.js
+++ main.js
@@ -2,13 +2,16 @@ const express = require('express');
 var cors = require('cors'); // import CORS model
 var session = require('express-session'); // set up session
 const usersRouter = require('./Routers/users');
+const connectDB = require('./configs/database')
+const mongoose = require('mongoose')
 
 var app = express();
 app.use(session({secret : 'my-secret'}));
 
 app.use(cors()); // prevent blocks of CORS policy (block request from unknown domain)
 
-require('./configs/database'); // run database.js on startup
+connectDB();
+
 
 
 
@@ -17,5 +20,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/users', usersRouter);
 
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(9000, () => console.log(`Server is running and listening on port 9000....`))
+})
 
-app.listen(9000, () => console.log("Server is running and listening on port 9000...."));
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
+
+
+// app.listen(9000, () => console.log("Server is running and listening on port 9000...."));
