c119bb855a980464cb22b7146c489b8e400f679c
giledrix <41838762+giledrix@users.noreply.github.com>
2023-01-17 14:52:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const express = require('express');
 var cors = require('cors'); // import CORS model
 var session = require('express-session'); // set up session
 const usersRouter = require('./Routers/users');
-const connectDB = require('./Configs/database')
 const mongoose = require('mongoose')
 const CONNECTION_URL = 'mongodb+srv://test_user123:test_user123@cluster0.kfrdiz3.mongodb.net/UsersDB?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 9000;
@@ -12,22 +11,14 @@ app.use(session({ secret: 'my-secret' }));
 
 app.use(cors()); // prevent blocks of CORS policy (block request from unknown domain)
 
-// connectDB();
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/users', usersRouter);
 
-app.use(cors()); // prevent blocks of CORS policy (block request from unknown domain)
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log('Server is running and listening on port 9000..')))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
-
-
-
-// app.listen(9000, () => console.log("Server is running and listening on port 9000...."));
