e369b1cc97f7add223f10f42b5d820fb8aa934b5
giledrix <41838762+giledrix@users.noreply.github.com>
2023-01-17 14:28:42 +0200

    Update moviesWebServiceDAL.js
    Ready to deploy

--- DAL/moviesWebServiceDAL.js
+++ DAL/moviesWebServiceDAL.js
@@ -2,19 +2,19 @@ const axios = require('axios');
 
 // get all movies from my movies web service
 const getAllMoviesFromWebService = function () {
-    return axios.get("http://localhost:8000/api/movies"); 
+    return axios.get("https://subscription-api.onrender.com/api/movies"); 
 }
 
 // get specific movie from my movies web service
 const getMovie = function (movieID) {
-    return axios.get("http://localhost:8000/api/movies/" + movieID); 
+    return axios.get("https://subscription-api.onrender.com/api/movies/" + movieID); 
 }
 
 
 // using post verb for add movie to web service
 const addNewMovie = async function (obj) {
 
-    let result = await axios.post('http://localhost:8000/api/movies', { Name: obj.Name, Genres: obj.Genres, Image: obj.Image, Premiered: obj.Premiered });
+    let result = await axios.post('https://subscription-api.onrender.com/api/movies', { Name: obj.Name, Genres: obj.Genres, Image: obj.Image, Premiered: obj.Premiered });
 
     return result.data;
 }
@@ -22,7 +22,7 @@ const addNewMovie = async function (obj) {
 // using delete verb - delete movie from web service
 const deleteMovie = async function (id) {
 
-    let result = await axios.delete('http://localhost:8000/api/movies/' + id);
+    let result = await axios.delete('https://subscription-api.onrender.com/api/movies/' + id);
 
     return result.data;
 }
@@ -30,7 +30,7 @@ const deleteMovie = async function (id) {
 //using put verb to update movie data in web service
 const updateMovie = async function (obj, movieID) {
 
-    let result = await axios.put('http://localhost:8000/api/movies/' + movieID, { Name: obj.Name, Genres: obj.Genres, Image: obj.Image, Premiered: obj.Premiered });
+    let result = await axios.put('https://subscription-api.onrender.com/api/movies/' + movieID, { Name: obj.Name, Genres: obj.Genres, Image: obj.Image, Premiered: obj.Premiered });
 
     return result.data;
 }
