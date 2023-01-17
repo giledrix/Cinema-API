18fb24918d3ce239121bb08663c2aac0cd478ad9
giledrix <41838762+giledrix@users.noreply.github.com>
2023-01-17 14:30:44 +0200

    Update subscriptionsWebServiceDAL.js
    ready to deploy

--- DAL/subscriptionsWebServiceDAL.js
+++ DAL/subscriptionsWebServiceDAL.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const subscriptionsWS_url = "http://localhost:8000/api/subscriptions"
+const subscriptionsWS_url = "https://subscription-api.onrender.com/api/subscriptions"
 
 const getTokenToSubscriptionWS = async function (id,timeout) {
     let  result = await axios.get(subscriptionsWS_url + "/getSubscriptionWSToken/" + id + "/" + timeout );
@@ -9,18 +9,18 @@ const getTokenToSubscriptionWS = async function (id,timeout) {
 
 // get all subscriptions from my subscriptions web service
 const getAllSubscriptions = function () {
-    return axios.get("http://localhost:8000/api/subscriptions");
+    return axios.get("https://subscription-api.onrender.com/api/subscriptions");
 }
 
 // get specific member from my subscriptions web service
 const getSubscription = function (id) {
-    return axios.get("http://localhost:8000/api/subscriptions/" + id);
+    return axios.get("https://subscription-api.onrender.com/api/subscriptions/" + id);
 }
 
 // using post verb to add member to web service
 const createSubscription = async function (obj) {
 
-    let result = await axios.post('http://localhost:8000/api/subscriptions', { MemberID: obj.MemberID, Movies: obj.Movies });
+    let result = await axios.post('https://subscription-api.onrender.com/api/subscriptions', { MemberID: obj.MemberID, Movies: obj.Movies });
 
     return result.data;
 }
@@ -28,7 +28,7 @@ const createSubscription = async function (obj) {
 //using put verb to update member data in web service
 const updateSubscription = async function (obj, memberID) {
 
-    let result = await axios.put('http://localhost:8000/api/subscriptions/' + memberID, { MemberID: obj.MemberID, Movies: obj.Movies });
+    let result = await axios.put('https://subscription-api.onrender.com/api/subscriptions/' + memberID, { MemberID: obj.MemberID, Movies: obj.Movies });
 
     return result.data;
 }
@@ -36,7 +36,7 @@ const updateSubscription = async function (obj, memberID) {
 // using delete verb - delete member from web service
 const deleteSubscription = async function (id) {
 
-    let result = await axios.delete('http://localhost:8000/api/subscriptions/' + id);
+    let result = await axios.delete('https://subscription-api.onrender.com/api/subscriptions/' + id);
 
     return result.data;
 }
