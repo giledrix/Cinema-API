beced3a55810a9bd50df6ff97eb42bd16560a5e1
giledrix <41838762+giledrix@users.noreply.github.com>
2023-01-17 14:27:44 +0200

    Update membersWebServiceDAL.js
    Ready to deploy

--- DAL/membersWebServiceDAL.js
+++ DAL/membersWebServiceDAL.js
@@ -2,19 +2,19 @@ const axios = require('axios');
 
 // get all members from my movies web service
 const getAllMembersFromWebService = function () {
-    return axios.get("http://localhost:8000/api/members");
+    return axios.get("https://subscription-api.onrender.com/api/members");
 }
 
 // get specific member from my movies web service
 const getMember = function (memberID) {
-    return axios.get("http://localhost:8000/api/members/" + memberID);
+    return axios.get("https://subscription-api.onrender.com/api/members/" + memberID);
 }
 
 
 // using post verb to add member to web service
 const addNewMember = async function (obj) {
 
-    let result = await axios.post('http://localhost:8000/api/members', { Name: obj.Name, Email: obj.Email, City: obj.City });
+    let result = await axios.post('https://subscription-api.onrender.com/api/members', { Name: obj.Name, Email: obj.Email, City: obj.City });
 
     return result.data;
 }
@@ -22,7 +22,7 @@ const addNewMember = async function (obj) {
 //using put verb to update member data in web service
 const updateMember = async function (obj, memberID) {
 
-    let result = await axios.put('http://localhost:8000/api/members/' + memberID, { Name: obj.Name, Email: obj.Email, City: obj.City });
+    let result = await axios.put('https://subscription-api.onrender.com/api/members/' + memberID, { Name: obj.Name, Email: obj.Email, City: obj.City });
 
     return result.data;
 }
@@ -30,7 +30,7 @@ const updateMember = async function (obj, memberID) {
 // using delete verb - delete member from web service
 const deleteMember = async function (id) {
 
-    let result = await axios.delete('http://localhost:8000/api/members/' + id);
+    let result = await axios.delete('https://subscription-api.onrender.com/api/members/' + id);
 
     return result.data;
 }
